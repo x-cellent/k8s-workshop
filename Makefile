a20f8b5a4c7ef610eb569553286f5f9d660161f7
Sandro Koll <sandro.koll@f-i-ts.de>
2022-04-06 15:57:21 +0200

    feat: Support PDF export

--- Makefile
+++ Makefile
@@ -5,14 +5,23 @@ default: build
 build:
 	docker run --rm -v $(shell pwd):/src golang:1.18 sh -c 'cd /src && go fmt ./... && go mod download && go mod tidy && go build -o bin/w6p'
 
-.PHONY .SILENT: slides
-slides: down
+.PHONY .SILENT: up
+up: down
 	docker run -d --name k8s-workshop-slides --net host -v $(shell pwd):/k8s-workshop golang:1.18 sh -c 'cd /k8s-workshop && go run static.go' >/dev/null
+
+.PHONY .SILENT: down
+down:
+	docker rm -f k8s-workshop-slides >/dev/null 2>&1 || true
+
+.PHONY .SILENT: slides
+slides: up
 	echo "Browse slides at http://localhost:8080 in 3 seconds..."
 	sleep 3
 	xdg-open http://localhost:8080
 	echo "Stop slides server with 'make down' or 'docker rm -f k8s-workshop-slides'"
 
-.PHONY .SILENT: down
-down:
-	docker rm -f k8s-workshop-slides >/dev/null 2>&1 || true
+.PHONY .SILENT: pdf
+pdf: up
+	sleep 3
+	docker run --rm -t --net host -v $(shell pwd):/docs astefanutti/decktape http://localhost:8080 /docs/slides.pdf
+	make --no-print-directory down
---
+++ cmd/slides/export/exportCmd.go
@@ -0,0 +1,82 @@
+package export
+
+import (
+	"embed"
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/x-cellent/k8s-workshop/cmd/slides/export/flag"
+	"github.com/x-cellent/k8s-workshop/cmd/slides/show"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"sync"
+	"time"
+)
+
+var Cmd = &cobra.Command{
+	Use:   "export",
+	Short: "Exports workshop slides to PDF",
+	RunE:  exportToPDF,
+}
+
+func init() {
+	Cmd.PreRun = func(cmd *cobra.Command, args []string) {
+		_ = viper.BindPFlags(Cmd.PersistentFlags())
+	}
+
+	Cmd.PersistentFlags().StringP(flag.DestinationPath, flag.DestinationPathShort, "./slides.pdf", "path to destination")
+}
+
+func exportToPDF(cmd *cobra.Command, args []string) error {
+	docker, err := exec.LookPath("docker")
+	if err != nil {
+		return err
+	}
+
+	port := 8080
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	go func() {
+		docs := cmd.Context().Value("docs").(embed.FS)
+		wg.Done()
+		err = show.Run("/docs", port, http.FS(docs), false)
+	}()
+	wg.Wait()
+	time.Sleep(time.Second)
+	if err != nil {
+		return err
+	}
+
+	dest := viper.GetString(flag.DestinationPath)
+	hostPath := filepath.Dir(dest)
+	if strings.HasPrefix(hostPath, "~") {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return err
+		}
+		hostPath = home + hostPath[1:]
+	} else if strings.HasPrefix(hostPath, ".") || !strings.HasPrefix(hostPath, string(filepath.Separator)) {
+		wd, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+		if strings.HasPrefix(hostPath, ".") {
+			hostPath = wd + hostPath[1:]
+		} else {
+			hostPath = filepath.Join(wd, hostPath)
+		}
+	}
+	filename := filepath.Base(dest)
+	vol := fmt.Sprintf("%s:/work", hostPath)
+	url := fmt.Sprintf("http://localhost:%d/docs", port)
+	out, err := exec.Command(docker, "run", "--rm", "-t", "--net", "host", "-v", vol, "astefanutti/decktape", url, filepath.Join("/work", filename)).CombinedOutput()
+	if err != nil {
+		fmt.Println(string(out))
+		return err
+	}
+
+	return exec.Command(docker, "rm", "-f", "k8s-slides").Run()
+}
